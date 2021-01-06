f465840cf7ef517271430f8dd3f796f47d44a4c4
NB Kelly <N.B.Kelly@protonmail.com>
2021-01-07 01:11:02 +1300

    Colorized output, debuglogger, output in general works. Progress is being made.

--- ConceptHelperV2.java
+++ ConceptHelperV2.java
@@ -20,6 +20,7 @@ public /*abstract*/ class ConceptHelperV2 {
 
     /** debug level */
     protected int _DEBUG_LEVEL = 0;
+    protected boolean _DEBUG = false; //true if _DEBUG_LEVEL > 0
     
     /** performs check-once analysis to enable colors */
     private boolean _COLOR_ENABLED() {
@@ -36,8 +37,8 @@ public /*abstract*/ class ConceptHelperV2 {
 	return _COLOR_ENABLED;
     }
     
-
-
+    private ConceptHelperV2 self = null;
+    
 
 
     
@@ -48,19 +49,51 @@ public /*abstract*/ class ConceptHelperV2 {
 	return new Command[0];
     }
 
-    protected void solveProblem() { }
+    protected void solveProblem() {
+	println("Normal line");
+	print("A");
+	print("B");
+	println(a2s(new char[]{'C', 'D'}));
+	//'C');
+
+	DEBUG("PLAIN DEBUG");
+	DEBUG(0, "DEBUG LEVEL 0");
+	DEBUG(1, "DEBUG LEVEL 1");
+	DEBUG(2, "DEBUG LEVEL 2");
+	DEBUGF(3, "DEBUG LEVEL %d%n", 3);
+	DEBUG(4, "DEBUG LEVEL 4");
+	DEBUG(5, "DEBUG LEVEL 5");
+	DEBUG(6, "DEBUG LEVEL 6");
+    }
 
 
 
-
-
+    public ConceptHelperV2() {
+	self = this;
+    }
+    
     
     // TODO: get rid of this
     public static void main(String[] argv) {
 	//first we set up the commands	
 	new ConceptHelperV2().run(argv); 
     }
-    
+
+    /**
+     * Runs the given program.
+     * <p>
+     * More specifically, we: <br>
+     * 1) Set all default commands <br>
+     * 2) *Add any additional user-based commands
+     * 3) Process all commands
+     * 4) Act based on the results of the default commands
+     * 5) *Act on the results of all user-input commands
+     * 6) Perform any pre-processing on the input
+     * 7) *Run the 'solveProblem' function. This is the actual user code.
+     *
+     * @param argv the argument vector for the program
+     * @since 1.0
+     */
     private void run(String[] argv) {
 	//first we set the default commands
 	setDefaultCommands();
@@ -97,7 +130,11 @@ public /*abstract*/ class ConceptHelperV2 {
 	_commands = res;
     }
 
-    //assume that all commands already exist : all we want to do is process them all
+    /**
+     * Process argument vector based on command set
+     * @param argv input arguments 
+     * @return any valid unprocessed arguments
+     */
     public String[] processCommands(String[] argv) {
 	int index = 0;
 	int index_last = -1;
@@ -148,16 +185,23 @@ public /*abstract*/ class ConceptHelperV2 {
 	return true;
     }
 
+    /**
+     * Prints text to the stderror using red and bold     
+     * @param str message to print
+     */
     private void PRINT_ERROR_TEXT(String str) {
 	System.err.println(Color.colorize(_COLOR_ENABLED(), str, Color.RED_BOLD));
     }
 
+    /**
+     * Fails with the given exit code
+     */
     private void FAIL(int exit) {
 	System.exit(exit);
     }
     
     /**
-     * Prints out the usage for all of the commands, and then gracefully exists with status code 1
+     * Prints out the usage for all of the commands, and then gracefully exists with given status code
      *
      * @param commands the set of all commands
      * @return exits the current program
@@ -177,27 +221,238 @@ public /*abstract*/ class ConceptHelperV2 {
 	System.exit(exit);
     }
 
+    /**
+     * Flattens an array into a string
+     * @param arr Array to flatten
+     * @return String representation of array
+     */
     private <T> String arrayToString(T[] arr) {
-	var res = new StringBuilder();
+	var res = new StringBuilder("[ ");
 	for(int i = 0; i < arr.length; i++)
 	    res.append(arr[i].toString() + " ");
 
-	return res.toString();
+	return res.toString() + "]";
     }
 
+    private String arrayToString(int[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(long[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(float[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(double[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(char[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(short[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(byte[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    private String arrayToString(boolean[] arr) {
+	var res = new StringBuilder("[ ");
+	for(int i = 0; i < arr.length; i++)
+	    res.append(arr[i] + " ");
+
+	return res.toString() + "]";
+    }
+
+    /**
+     * Cuts the input vector to a smaller size
+     * @param arr the input vector
+     * @param cutAt index to cut the vector at
+     * @return cut argvector
+     */
     private String[] _REMAINING_ARGUMENTS(String[] arr, int cutAt) {
 	if(cutAt == arr.length)
 	    return new String[0];
-
+	if(cutAt == 0)
+	    return arr;
+	
 	return Arrays.copyOfRange(arr, cutAt, arr.length);
     }
 
 
 
+    /*********************************************************
+     *
+     *                    OUTPUT COMMANDS
+     *
+     *********************************************************/
+    
+    public DebugLogger logger = new DebugLogger() {
+	    public void DEBUG(String s) { self.DEBUG(s); }
+	    public void DEBUG(int level, Object s) { self.DEBUG(1, s); }
+	    public void DEBUGF(int level, String s, Object... args) { self.DEBUGF(level, s, args); }
+
+	    public void print(Object value) { self.print(value); }
+	    public void printf(String value, Object... args) { self.printf(value, args); }
+	    public void println(Object value) { self.println(value); }
+	};	    
+    
+    //DEBUG (int level, String s)
+    //DEBUG (String s) -> DEBUG(1, s)
+    //DEBUGF(int level, String s, args) -> DEBUG(level, f(s, args))
+    //DEBUGF(String s, args) -> DEBUGF(1, s, args)
+    //
+    //print
+    //printf
+    //println
+    //
+    //a2s - > arrayToString
+    
+    //colorize based on debug level
+    //1 = black
+    //2 = bold black
+    //3 = yellow
+    //4 = red
+    //5 = bold red
+    private Color _DEBUG_TO_COLOR(int level) {
+	switch(level) {
+	case 0:
+	case 1:
+	    return Color.RESET;
+	case 2:
+	    return Color.GREEN;
+	case 3:
+	    return Color.YELLOW;
+	case 4:
+	    return Color.RED;
+	case 5:
+	    return Color.RED_BOLD;
+	default:
+	    return Color.BLACK;
+	}
+    }
+    
+    public void DEBUG(int level, Object message) {
+	if(_DEBUG_LEVEL == 0)
+	    return;
+
+	//we print anything with a level equal to or below our own
+	if(_DEBUG_LEVEL >= level)
+	    System.err.println(_DEBUG_COLORIZE(message.toString(), _DEBUG_TO_COLOR(level)));
+    }
+
+    public void DEBUG(Object message) {
+	DEBUG(1, message);
+    }
+
+    public void DEBUGF(int level, String message, Object... args) {
+	if(_DEBUG_LEVEL == 0)
+	    return;
+
+	if(_DEBUG_LEVEL == 0)
+	    return;
+
+	String tmp = String.format(message, args);
+	
+	//we print anything with a level equal to or below our own
+	if(_DEBUG_LEVEL >= level)
+	    System.err.print(_DEBUG_COLORIZE(tmp, _DEBUG_TO_COLOR(level)));
+    }
+
+    public void DEBUGF(String message, Object... args) {
+	DEBUGF(1, message, args);
+    }
+
+    private String _DEBUG_COLORIZE(String s, Color c) {
+	return Color.colorize(_COLOR_ENABLED(), s, c);
+    }
+    
+    public void print(Object a) {
+	System.out.print(a);
+    }
+
+    public void printf(String a, Object... args) {
+	System.out.printf(a, args);
+    }
+
+    public void println(Object a) {
+	System.out.println(a);
+    }
+
+
+    //array to str
 
     
+    public String a2s(Object[] a) {
+	return arrayToString(a);
+    }
 
+    public String a2s(int[] a) {
+	return arrayToString(a);
+    }
 
+    public String a2s(long[] a) {
+	return arrayToString(a);
+    }
+
+    public String a2s(float[] a) {
+	return arrayToString(a);
+    }
+
+    public String a2s(double[] a) {
+	return arrayToString(a);
+    }
+
+    public String a2s(boolean[] a) {
+	return arrayToString(a);
+    }
+
+    public String a2s(byte[] a) {
+	return arrayToString(a);
+    }
+
+    public String a2s(short[] a) {
+	return arrayToString(a);
+    }
+
+    public String a2s(char[] a) {
+	return arrayToString(a);
+    }
+        
 
     /*********************************************************
      *
@@ -241,5 +496,8 @@ public /*abstract*/ class ConceptHelperV2 {
 	_PAGE_ENABLED = (_page.matched > 0);
 	_INTERACTIVE_MODE = (_interactive.matched > 0);
 	_DEBUG_LEVEL = (_debugLevel.matched > 0 ? _debugLevel.getValue() : 0);
+
+	//if the debug level is greater than 0, then debug mode as a whole is enabled
+	_DEBUG = _DEBUG_LEVEL > 0;
     }
 }
