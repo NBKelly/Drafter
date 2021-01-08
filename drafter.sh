cd65f3e806ec81d99679065b71abd35f16f4155c
NB Kelly <N.B.Kelly@protonmail.com>
2021-01-09 11:21:30 +1300

    check class actually compiles

--- drafter.sh
+++ drafter.sh
@@ -6,4 +6,9 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 #-classpath lets us keep the directories we supply, while making the resources load relative
 # to the java class.
 javac -classpath "$SCRIPTPATH" com/nbkelly/working/MakeHelper.java
-java -classpath "$SCRIPTPATH" com.nbkelly.working.MakeHelper "$@"
+if [ $? -ne 0 ]; then
+    echo "Could not compile base class"
+    exit
+else
+    java -classpath "$SCRIPTPATH" com.nbkelly.working.MakeHelper "$@"
+fi
