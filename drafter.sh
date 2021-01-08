8b004ab9f9848a47ec9eb8944883f9ccc828b5ed
NB Kelly <N.B.Kelly@protonmail.com>
2021-01-09 11:19:34 +1300

    documentation

--- drafter.sh
+++ drafter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#stackoverflow says this is the best way to get the path...
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+#-classpath lets us keep the directories we supply, while making the resources load relative
+# to the java class.
 javac -classpath "$SCRIPTPATH" com/nbkelly/working/MakeHelper.java
 java -classpath "$SCRIPTPATH" com.nbkelly.working.MakeHelper "$@"
