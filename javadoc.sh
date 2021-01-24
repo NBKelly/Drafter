048df9ae70af97666af812b7f326c48d11013302
NB Kelly <N.B.Kelly@protonmail.com>
2021-01-25 11:20:13 +1300

    moved javadocs

--- javadoc.sh
+++ javadoc.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# clear an old docs
-rm -r docs/javadocs
-
 #generate docs
-javadoc -link https://docs.oracle.com/en/java/javase/11/docs/api/ -d docs/javadocs com.nbkelly com.nbkelly.working
+javadoc -link https://docs.oracle.com/en/java/javase/11/docs/api/ -d docs com.nbkelly com.nbkelly.working
